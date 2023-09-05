2e1a2d2ea8a0ccab3bc9201f4dee3233e120cdb5
GanaSasank <1nh19me032.ganasasankreddy@gmail.com>
2023-09-05 11:48:57 +0530

    Added three files

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require("http");
+var fs = require("fs");
+var dt = require("./datatime");
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.write("Today date and time is " + dt.myDateTime());
+    res.write("Hello World");
+    res.write(req.url);
+    // fs.appendFile("./sample.txt", "I am Gana", function (err) {
+    //   if (err) return err;
+    //   console.log("Wrote!!!!!!!!!!");
+    // });
+    fs.unlink("./sample.txt", function (err) {
+      if (err) return err;
+      console.log("Deleted");
+    });
+    res.end();
+  })
+  .listen(8080);
